5f2fe60e8525d571af133e7820bc3cff0c4989a8
Ole Martin Ruud <barskern@outlook.com>
2018-09-27 02:46:53 +0200

    Add dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,18 @@
-extern crate serde;
-extern crate serde_derive;
+#![feature(try_from)]
 
-pub mod internal;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate failure;
+#[macro_use]
+extern crate failure_derive;
+#[macro_use]
+extern crate lazy_static;
+extern crate chrono;
+extern crate regex;
+
 pub mod external;
+pub mod internal;
+pub mod payloads;
+pub mod raw;
+pub mod valid;
