1fca79fd15897b733504f49291b097e8fd24ad3f
Yngve Hestem <yjhestem@stud.ntnu.no>
2018-10-03 23:27:27 +0200

    Trying to fix Token

--- src/lib.rs
+++ src/lib.rs
@@ -13,8 +13,7 @@ extern crate lazy_static;
 extern crate chrono;
 extern crate regex;
 
-// TODO should probably be changed to something else
-pub type Token = u32;
+
 
 #[macro_use]
 pub mod macros;
